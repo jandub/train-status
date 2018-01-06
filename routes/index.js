2d5da3777cb021d838344ca331336538cbde6990
Jan Dubina <jan@azavista.com>
2018-01-06 14:34:19 +0100

    Code formatting changes.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import path from 'path';
 
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.sendFile(path.resolve('views/index.html'));
 });
+
 
 export default router;
