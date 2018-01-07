2f7f72c59f385ee53d16dcaf225dc0b18be8c76e
Jan Dubina <jan@azavista.com>
2018-01-07 21:16:50 +0100

    Generate socket.io events when the train status gets updated.

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 import express from 'express';
 import path from 'path';
 import logger from 'morgan';
+import socketio from 'socket.io';
 
 import index from './routes/index';
 
 import eventProcInit from './libs/event_proc_init';
 
 
+// initialize express
 const app = express();
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 
+// initialize socket.io, gets attached to the server in /bin/www
+const io = socketio();
+app.io = io;
+
 // start generating and processing train status events
-eventProcInit();
+eventProcInit(io);
 
 
 export default app;
--- libs/event_proc_init.js
+++ libs/event_proc_init.js
@@ -3,12 +3,40 @@ import TrainStatusManager from './train_status_manager.js';
 
 
 const trainStatus = new TrainStatusManager();
+let io;
 
-const handleEvent = (status) => {
-    const updateResult = trainStatus.update(status);
+/**
+ *  Callback. Updates the train status and pushes the change to all
+ *  listening clients through socket.io.
+ *
+ *  @param  {Object} statusUpdate           The update object.
+ *  @param  {String} statusUpdate.train     Name of the train.
+ *  @param  {String} statusUpdate.action    Updated action of the train.
+ */
+const handleEvent = status => {
+    const trainData = trainStatus.update(status);
+    const updateData = {
+        'train': status.train,
+        'data': trainData
+    };
+
+    io.emit('update', updateData);
 }
 
-const initialize = (avgDelay = 5000) => {
+/**
+ *  Sends current status of all trains to newly connected socket.io
+ *  clients. Also, it starts the event generator.
+ *
+ *  @param  {Object} socketio   Socket.io instance.
+ *  @param  {Number} avgDelay   Average delay between train updates.
+ */
+const initialize = (socketio, avgDelay = 5000) => {
+    io = socketio;
+
+    io.on('connection', client => {
+        client.emit('init_update', trainStatus.statuses);
+    });
+
     subscribeEvents(handleEvent, avgDelay);
 }
 
